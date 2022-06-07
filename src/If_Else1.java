dffd7add8df3a8dddfbfc91c29d948bca58b93bd
Subrat Kumar Dang <w.subrat@gmail.com>
2022-06-07 11:17:45 +0200

    First Commit..

---
+++ src/If_Else1.java
@@ -0,0 +1,17 @@
+
+
+public class If_Else1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//decision making statement 
+		int time = 20;
+		if (time < 18) {
+		  System.out.println("Good day.");
+		} else {
+		  System.out.println("Good evening.");
+		}
+	}
+
+}
